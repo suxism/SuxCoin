891927ab3f18939702ab7208edd88b600479f1d8
suxism <40383463+suxism@users.noreply.github.com>
2021-03-26 18:10:06 +0900

    Update base58.h

--- src/base58.h
+++ src/base58.h
@@ -277,7 +277,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 32, 
+        PUBKEY_ADDRESS = 25, 
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
